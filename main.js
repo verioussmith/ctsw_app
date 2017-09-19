4d31e16f490a82e8a1ecdc71a7d80e98d52895d0
Verious Smith III <verious.smith@philoveracity.com>
2017-09-19 10:08:45 -0700

    Complete function for application

--- main.js
+++ main.js
@@ -3,4 +3,22 @@ function fetchIssues() {
 	var issuesList = document.getElementByID('issuesList');
 
 	issuesList.innerHTML = '';
+
+	for ( var i = 0; i < issues.length; i++) {
+		var id = issues[i].id;
+		var desc = issues[i].description;
+		var severity = issues[i].severity;
+		var assignedTo = issues[i].assignedTo;
+		var status = issues[i].status;
+
+		issuesList.innerHTML +=		'<div class="well">'+
+									'<h6>Issue ID:' + id + '</h6>'+
+									'<p><span class="label label-info">' + status + '</span></p>'+
+									'<h3>' + desc + '</h3>'+
+									'<p><span class="glyphicon glyphicon-time"></span>' + severity + '</p>'+
+									'<p><span class="glyphicon glyphicon-user"></span>' + assignedTo + '</p>'+
+									'<a href="#" onClick="setStatusClosed(\''+id+'\')" class="btn btn-warning">Close</a>'+
+									'<a href="#" onClick="deleteIssue(\''+id+'\')" class="btn btn-danger">Delete</a>'+
+									'</div>';
+	}
 }
